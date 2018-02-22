36727d3d5685b1bb25cf25d5bd289141bfa9b9ac
Erika & Koike <erika.takata@gmail.com>
2018-02-22 19:45:08 -0300

    Administrador cadastra cozinhas

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -2,4 +2,20 @@ class CuisinesController < ApplicationController
   def show
     @cuisine = Cuisine.find(params[:id])
   end
+
+  def new
+    @cuisine = Cuisine.new
+  end
+
+  def create
+    cuisine_params = params.require(:cuisine).permit(:name)
+    @cuisine = Cuisine.new(cuisine_params)
+
+    if @cuisine.save
+      redirect_to cuisine_path(@cuisine)
+    else
+      flash[:error] = 'Você deve informar o nome da cozinha'
+      render :new
+    end
+  end
 end
